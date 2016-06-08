bd5992b1a60181b3fe5d7236bc49f297d4ab9353
Dmytro.Verbovyi <Dmytro.Verbovyi@eleks.com>
2016-06-08 20:17:33 +0300

    add dependencies

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+angular.module('demoModule', ['heremaps'])
+    .config(["HereMapsConfigProvider", function (HereMapsConfigProvider) {
+        HereMapsConfigProvider.setOptions({
+            'app_id': 'your_app_id_here',
+            'app_code': 'your_app_code_here',
+            'useHTTPS': true
+        });
+    }]);
